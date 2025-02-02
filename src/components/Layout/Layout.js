b37a4ab363414855bd411275c720055aade4e89e
Dhanasriganesh <dhanasriganesh9@gmail.com>
2025-02-02 19:24:09 +0530

    fo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import Header from '../Header/Header';
 import Routers from '../../Routers/Routers';
-import Footer from '../Footer/Footer';
 function Layout() {
   return (
     <Router> {/* Wrap your component in Router */}
